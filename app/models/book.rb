c45fa2625a23d05a41e1e2486b6139741714624f
Josh Young <j22cal@gmail.com>
2018-06-30 14:27:20 -0500

    Write the minimal amount of code for the tests to pass

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+  validates :title, presence: true
+  validates :word_count, presence: true, numericality: { only_integer: true }
+  validates :description, presence: true
 end
