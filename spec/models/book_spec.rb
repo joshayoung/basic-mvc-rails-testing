96afcdcbc3ae3583c8604734f253f240e6447b4b
Josh Young <j22cal@gmail.com>
2018-06-30 14:14:46 -0500

    Start with failing model specs

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,35 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validation tests" do
+    #book = Book.new(title: "Gone with the Wind", word_count: "418053", description: "A famous novel.")
+    it "require a title" do
+      book = Book.new(word_count: "418053", description: "A famous novel.").save
+      expect(book).to eq(false)
+    end
+
+    it "require a word_count" do
+      book = Book.new(title: "Gone with the Wind", description: "A famous novel.").save
+      expect(book).to eq(false)
+    end
+
+    it "require a word_count to be numeric" do
+      book = Book.new(title: "Gone with the Wind", word_count: "one thousand", description: "A famous novel.")
+      book.valid?
+      expect(book.errors[:word_count].any?).to eq (true)
+    end
+
+    it "require a description" do
+      book = Book.new(title: "Gone with the Wind", word_count: "418053").save
+      expect(book).to eq(false)
+    end
+    it "save successfully" do
+      book = Book.new(
+        title: "Gone with the Wind",
+        word_count: "418053",
+        description: "A famous novel."
+      ).save
+      expect(book).to eq(true)
+    end
+  end
 end
