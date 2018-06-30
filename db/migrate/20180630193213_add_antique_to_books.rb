70f894ce76576b641e5215717023e0cae0243fb6
Josh Young <j22cal@gmail.com>
2018-06-30 14:33:35 -0500

    Add field to table

---
+++ db/migrate/20180630193213_add_antique_to_books.rb
@@ -0,0 +1,5 @@
+class AddAntiqueToBooks < ActiveRecord::Migration[5.2]
+  def change
+    add_column :books, :antique, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_30_160419) do
+ActiveRecord::Schema.define(version: 2018_06_30_193213) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_06_30_160419) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "antique"
   end
 
 end
