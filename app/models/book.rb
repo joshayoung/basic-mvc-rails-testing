5ba899be9669424bc22d11d704496d5fd39cf21a
Josh Young <j22cal@gmail.com>
2018-06-30 15:48:18 -0500

    Add code so tests will pass

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,13 @@ class Book < ApplicationRecord
   validates :title, presence: true
   validates :word_count, presence: true, numericality: { only_integer: true }
   validates :description, presence: true
+
+  class << self
+    def antiques
+      where(antique: true)
+    end
+    def recent
+      where(antique: false)
+    end
+  end
 end
