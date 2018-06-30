6cd5a89cb4c668e5287b06b0ec79f5ca5d9401c7
Josh Young <j22cal@gmail.com>
2018-06-30 14:45:28 -0500

    Set failing model method tests

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -2,7 +2,6 @@ require "rails_helper"
 
 RSpec.describe Book, type: :model do
   context "validation tests" do
-    #book = Book.new(title: "Gone with the Wind", word_count: "418053", description: "A famous novel.")
     it "require a title" do
       book = Book.new(word_count: "418053", description: "A famous novel.").save
       expect(book).to eq(false)
@@ -16,7 +15,7 @@ RSpec.describe Book, type: :model do
     it "require a word_count to be numeric" do
       book = Book.new(title: "Gone with the Wind", word_count: "one thousand", description: "A famous novel.")
       book.valid?
-      expect(book.errors[:word_count].any?).to eq (true)
+      expect(book.errors[:word_count].any?).to eq(true)
     end
 
     it "require a description" do
@@ -31,5 +30,23 @@ RSpec.describe Book, type: :model do
       ).save
       expect(book).to eq(true)
     end
+  end
+
+  context "method tests" do
+    let(:params) { { title: "Gone with the Wind", word_count: "418053", description: "A famous novel." } }
+    before(:each) do
+      Book.new(params).save
+      Book.new(params.merge(antique: true)).save
+      Book.new(params.merge(antique: false)).save
+    end
+
+    it "will return the antique books" do
+      expect(Book.antiques.size).to eq(1)
+    end
+
+    it "will return the newer books" do
+      expect(Book.recent.size).to eq(2)
+    end
+
   end
 end
