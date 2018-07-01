fae6614ce77c8d5214babf034692b0285c51852f
Josh Young <j22cal@gmail.com>
2018-07-01 15:55:07 -0500

    Make code changes to allow feature tests to pass

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,53 @@
 class BooksController < ApplicationController
-  def index; end
-  def show; end
-  def new; end
-  def edit; end
-  def create; end
-  def update; end
-  def destroy; end
+  def index
+    @books = Book.all
+  end
+
+  #Book Controller
+  def show
+    @book = Book.find_by(id: params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def edit
+    @book = Book.find_by(id: params[:id])
+  end
+
+  def create
+    @book = Book.new(parameters)
+    if @book.save
+      flash[:notice] = "Book successfully created"
+      redirect_to books_url
+    else
+      render :new
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(parameters)
+      flash[:notice] = "Book successfully updated!"
+      redirect_to(books_url)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Book.find(params[:id]).destroy
+    redirect_to(books_url)
+  end
+
+private
+
+  def parameters
+    {
+      title: params[:book][:title],
+      word_count: params[:book][:word_count],
+      description: params[:book][:description]
+    }
+  end
 end
