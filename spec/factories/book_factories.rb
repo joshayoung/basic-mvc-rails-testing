1a4907d0dcc9bd802aaa57fded5df0e88cbbf965
Josh Young <j22cal@gmail.com>
2020-02-27 09:09:17 -0600

    Upgrade ruby version and correct factory error

--- spec/factories/book_factories.rb
+++ spec/factories/book_factories.rb
@@ -2,8 +2,8 @@ require 'faker'
 
 FactoryBot.define do
   factory :book do
-    title Faker::Book.title
-    word_count Faker::Number.number(10)
-    description "A famous novel"
+    title { Faker::Book.title }
+    word_count { Faker::Number.number(10) }
+    description { "A famous novel" }
   end
 end
